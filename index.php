fc04d3c73f5b0bf5965443809421c807fbc4ef91
Karunov <karunov@list.ru>
2016-07-05 23:03:49 +0300

    главная стр и стр со статьей

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Главная</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container text-center menu">
+      <h1 >Главная страница блога</h1>
+    
+    <div class="btn-group" role="group" aria-label="...">
+      <a href="#" class="btn btn-default">Главная</a>
+      <a href="#" class="btn btn-default">Посты</a>
+      <a href="#" class="btn btn-default">еще ссылка</a>
+    </div>
+    
+    </div>
+
+    <div class="container">
+      <div class="col-md-6">
+      <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Категория статей 1</h3>
+          </div>
+          <div class="panel-body">
+            <p><a href="">статья 1</a></p>
+            <p><a href="">статья 2</a></p>
+            <p><a href="">статья 3</a></p>
+          </div>
+      </div>
+      </div>
+
+      <div class="col-md-6">
+      <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Категория статей 2</h3>
+          </div>
+          <div class="panel-body">
+            <p><a href="">статья 1</a></p>
+            <p><a href="">статья 2</a></p>
+            <p><a href="">статья 3</a></p>
+          </div>
+      </div>
+      </div>
+    </div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ posts.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>страница со статьей</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container text-center menu">
+      <h1 >Статья</h1>
+    
+    <div class="btn-group" role="group" aria-label="...">
+      <a href="#" class="btn btn-default">Главная</a>
+      <a href="#" class="btn btn-default">Посты</a>
+      <a href="#" class="btn btn-default">еще ссылка</a>
+    </div>
+    
+    </div>
+
+    <div class="container">
+     
+      <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Статья номер...</h3>
+          </div>
+          <div class="panel-body">
+            <p>Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации, например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь. Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите какой-нибудь шутки, скрытой в середине абзаца. Также все другие известные генераторы Lorem Ipsum используют один и тот же текст, который они просто повторяют, пока не достигнут нужный объём. Это делает предлагаемый здесь генератор единственным настоящим Lorem Ipsum генератором. Он использует словарь из более чем 200 латинских слов, а также набор моделей предложений. В результате сгенерированный Lorem Ipsum выглядит правдоподобно, не имеет повторяющихся абзацей или "невозможных" слов.</p>
+           
+          </div>
+      </div>
+   
+
+     
+    </div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
